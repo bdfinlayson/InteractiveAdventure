478e9198969772a68522629c39dd7330cac84d92
Bryan Finlayson <bryan.finlayson@metova.com>
2017-01-08 09:54:29 -0600

    add view controller

---
+++ InteractiveStory/PageController.swift
@@ -0,0 +1,57 @@
+//
+//  PageController.swift
+//  InteractiveStory
+//
+//  Created by Bryan Finlayson on 1/8/17.
+//  Copyright © 2017 Bryan Finlayson. All rights reserved.
+//
+
+import UIKit
+
+class PageController: UIViewController {
+    
+    var page: Page?
+    
+    let artwork = UIImageView()
+    let storyLabel = UILabel()
+    let firstChoiceButton = UIButton(type: .System)
+    let secondChoiceButton = UIButton(type: .System)
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    init(page: Page) {
+        self.page = page
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+                
+        if let page = page {
+            print(page.story.text)
+            artwork.image = page.story.artwork
+        }
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillLayoutSubviews() {
+        view.addSubview(artwork)
+        artwork.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activateConstraints([
+            artwork.topAnchor.constraintEqualToAnchor(view.topAnchor),
+            artwork.bottomAnchor.constraintEqualToAnchor(view.bottomAnchor),
+            artwork.rightAnchor.constraintEqualToAnchor(view.rightAnchor),
+            artwork.leftAnchor.constraintEqualToAnchor(view.leftAnchor)
+        ])
+    }
+
+}
